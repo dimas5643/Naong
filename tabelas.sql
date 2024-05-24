96e958358c7ebdc4b1cb51a4be96e7cfbc5912e0
Bruno Dimas <ndbruno564@gmail.com>
2024-05-24 20:31:07 -0300

    tabelas

---
+++ tabelas.sql
@@ -0,0 +1,71 @@
+CREATE TABLE `ongs` (
+  `id_ong` int(11) NOT NULL,
+  `cpnj` varchar(45) NOT NULL,
+  `razao_social` varchar(45) DEFAULT NULL,
+  `nome_fantasia` varchar(45) DEFAULT NULL,
+  `endereco` varchar(45) DEFAULT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  `cep` varchar(45) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  `pais` varchar(45) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `data_cadastro` datetime DEFAULT NULL,
+  `ativo` char(1) DEFAULT NULL,
+  `senha` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_ong`,`cpnj`,`email`)
+)
+
+CREATE TABLE `doadores` (
+  `id_doador` int(11) NOT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  `cpf_cnpj` varchar(45) NOT NULL,
+  `cidade` varchar(45) DEFAULT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  `cep` varchar(45) DEFAULT NULL,
+  `pais` varchar(45) DEFAULT NULL,
+  `telefone` varchar(45) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `senha` varchar(45) DEFAULT NULL,
+  `nascimento` date DEFAULT NULL,
+  `cadastro` datetime DEFAULT NULL,
+  `endereco` varchar(45) DEFAULT NULL,
+  `ativo` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id_doador`,`email`,`cpf_cnpj`)
+) 
+
+CREATE TABLE `departamentos` (
+  `id_departamento` int(11) NOT NULL,
+  `nome_departamento` varchar(45) DEFAULT NULL,
+  `ativo` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id_departamento`)
+)
+
+CREATE TABLE `necessidades` (
+  `id_necessidade` int(11) NOT NULL,
+  `id_ong` int(11) DEFAULT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  `descricao` text DEFAULT NULL,
+  `data` datetime DEFAULT NULL,
+  `status` char(1) DEFAULT NULL,
+  `quantidade` float DEFAULT NULL,
+  PRIMARY KEY (`id_necessidade`),
+  KEY `fk_necessidade_ong_idx` (`id_ong`),
+  KEY `fk_necessidade_departamento_idx` (`id_departamento`),
+  CONSTRAINT `fk_necessidade_departamento` FOREIGN KEY (`id_departamento`) REFERENCES `departamentos` (`id_departamento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_necessidade_ong` FOREIGN KEY (`id_ong`) REFERENCES `ongs` (`id_ong`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)
+
+CREATE TABLE `registro_doacoes` (
+  `id_registro` int(11) NOT NULL,
+  `id_doador` int(11) DEFAULT NULL,
+  `id_ong` int(11) DEFAULT NULL,
+  `doacao` varchar(45) DEFAULT NULL,
+  `data_doacao` datetime DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  PRIMARY KEY (`id_registro`),
+  KEY `idx_registro_ong` (`id_doador`),
+  KEY `fk_registro_ong_idx` (`id_ong`),
+  CONSTRAINT `fk_registro_doador` FOREIGN KEY (`id_doador`) REFERENCES `doadores` (`id_doador`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_registro_ong` FOREIGN KEY (`id_ong`) REFERENCES `ongs` (`id_ong`) ON DELETE NO ACTION ON UPDATE NO ACTION
+)

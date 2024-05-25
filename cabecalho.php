bed46988a6d7690512533bc344c23cbe4d8ea3fd
João Vitor <joaov.cercena@gmail.com>
2024-05-25 15:28:32 -0300

    tela de cadastro e login

---
+++ cabecalho.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+  <meta charset="utf-8" />
+  <title>NaOng</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+  <meta content="" name="keywords" />
+  <meta content="" name="description" />
+
+  <!-- Google Web Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet" />
+
+  <!-- Icon Font Stylesheet -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+
+  <!-- Libraries Stylesheet -->
+  <link href="lib/animate/animate.min.css" rel="stylesheet" />
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+
+  <!-- Customized Bootstrap Stylesheet -->
+  <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+  <!-- Template Stylesheet -->
+  <link href="css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+  <!-- Spinner Start -->
+  <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+  <!-- Spinner End -->
+
+  <!-- Topbar Start -->
+  <!-- <div class="container-fluid bg-dark px-5 d-none d-lg-block">
+    <div class="row gx-0 align-items-center" style="height: 45px">
+      <div class="col-lg-8 text-center text-lg-start mb-lg-0">
+        <div class="d-flex flex-wrap">
+          <a href="#" class="text-light me-4"><i class="fas fa-map-marker-alt text-primary me-2"></i>Find A
+            Location</a>
+          <a href="#" class="text-light me-4"><i class="fas fa-phone-alt text-primary me-2"></i>+01234567890</a>
+          <a href="#" class="text-light me-0"><i class="fas fa-envelope text-primary me-2"></i>Example@gmail.com</a>
+        </div>
+      </div>
+      <div class="col-lg-4 text-center text-lg-end">
+        <div class="d-flex align-items-center justify-content-end">
+          <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-3"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-3"><i class="fab fa-twitter"></i></a>
+          <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-3"><i class="fab fa-instagram"></i></a>
+          <a href="#" class="btn btn-light btn-square border rounded-circle nav-fill me-0"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+      </div>
+    </div>
+  </div> -->
+  <!-- Topbar End -->
+
+  <!-- Navbar & Hero Start -->
+  <div class="container-fluid position-relative p-0">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-4 px-lg-5 py-3 py-lg-0">
+      <a href="index.html" class="navbar-brand p-0">
+        <h1 class="text-primary m-0"><i class="fas fa-star-of-life me-3"></i>NaOng</h1>
+        <!-- <img src="img/logo.png" alt="Logo"> -->
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="fa fa-bars"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0">
+          <a href="index.html" class="nav-item nav-link">Home</a>
+          <a href="about.html" class="nav-item nav-link">About</a>
+          <a href="service.html" class="nav-item nav-link">Services</a>
+          <div class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+            <div class="dropdown-menu m-0">
+              <a href="appointment.html" class="dropdown-item">Appointment</a>
+              <a href="feature.html" class="dropdown-item">Features</a>
+              <a href="blog.html" class="dropdown-item">Our Blog</a>
+              <a href="team.html" class="dropdown-item">Our Team</a>
+              <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+              <a href="404.html" class="dropdown-item">404 Page</a>
+            </div>
+          </div>
+          <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+        </div>
+        <a href="#" class="btn btn-primary rounded-pill text-white py-2 px-4 flex-wrap flex-sm-shrink-0">Book Appointment</a>
+      </div>
+    </nav>
+  </div>
+  <!-- Navbar End -->
---
+++ cadastro.php
@@ -0,0 +1,193 @@
+<?php include('./cabecalho.php') ?>
+<!-- Book Appointment Start -->
+<div class="container-fluid appointment py-12">
+    <div class="container py-12" >
+        <div class="row g-12 align-items-center">
+
+
+            <div class="col-lg-6">
+                <div class="col-lg-12 wow fadeInRight" data-wow-delay="0.4s">
+                    <div class="appointment-form rounded p-5">
+                        <p class="fs-4 text-uppercase text-primary">Faça o Login</p>
+                        <h1 class="display-5 mb-4">Login</h1>
+                        <form>
+                            <div class="row gy-3 gx-4">
+                                <div class="col-xl-12">
+                                    <label for="">EMAIL</label>
+                                    <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="EMAIL">
+                                </div>
+                                <div class="col-xl-12">
+                                    <label for="">SENHA</label>
+                                    <input type="text" class="form-control py-3 border-primary bg-transparent" placeholder="SENHA">
+                                </div>
+                                <div class="col-12">
+                                    <button type="button" class="btn btn-primary text-white w-100 py-3 px-5">LOGIN</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6"  style="padding-top: 50px; margin-top: 50px;">
+                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="pills-doador-tab" data-bs-toggle="pill" data-bs-target="#pills-doador" type="button" role="tab" aria-controls="pills-doador" aria-selected="true">Doador</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="pills-ong-tab" data-bs-toggle="pill" data-bs-target="#pills-ong" type="button" role="tab" aria-controls="pills-ong" aria-selected="false">ONG</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade show active" id="pills-doador" role="tabpanel" aria-labelledby="pills-doador-tab">
+                        <div class="col-lg-12 wow fadeInRight" data-wow-delay="0.4s">
+                            <div class="appointment-form rounded p-5">
+                                <p class="fs-4 text-uppercase text-primary">Preencha o formulario</p>
+                                <h1 class="display-5 mb-4">Cadastro de Doador</h1>
+                                <form>
+                                    <div class="row gy-3 gx-4">
+
+                                        <div class="col-xl-6">
+                                            <label for="">NOME</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="NOME">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">DOCUMENTO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="CPF/CNPJ">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">CEP</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="CEP">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">ESTADO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="ESTADO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">CIDADE</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="CIDADE">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">ENDEREÇO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="ENDEREÇO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">CONTATO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent" placeholder="CONTATO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">EMAIL</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="EMAIL">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">SENHA</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent" placeholder="SENHA">
+                                        </div>
+                                        <!-- <div class="col-xl-6">
+                                        <select class="form-select py-3 border-primary bg-transparent" aria-label="Default select example">
+                                            <option selected>Your Gender</option>
+                                            <option value="1">Male</option>
+                                            <option value="2">FeMale</option>
+                                            <option value="3">Others</option>
+                                        </select>
+                                    </div> -->
+                                        <div class="col-xl-6">
+                                            <label for="">DATA DE NASCIMENTO</label>
+                                            <input type="date" class="form-control py-3 border-primary bg-transparent">
+                                        </div>
+                                        <!-- <div class="col-xl-6">
+                                        <select class="form-select py-3 border-primary bg-transparent" aria-label="Default select example">
+                                            <option selected>Department</option>
+                                            <option value="1">Physiotherapy</option>
+                                            <option value="2">Physical Helth</option>
+                                            <option value="2">Treatments</option>
+                                        </select>
+                                    </div> -->
+                                        <!-- <div class="col-12">
+                                        <textarea class="form-control border-primary bg-transparent text-white" name="text" id="area-text" cols="30" rows="5" placeholder="Write Comments"></textarea>
+                                    </div> -->
+                                        <div class="col-12">
+                                            <button type="button" class="btn btn-primary text-white w-100 py-3 px-5">CADASTRAR</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="pills-ong" role="tabpanel" aria-labelledby="pills-ong-tab">
+                        <div class="col-lg-12 wow fadeInRight" data-wow-delay="0.4s">
+                            <div class="appointment-form rounded p-5">
+                                <p class="fs-4 text-uppercase text-primary">Preencha o formulario</p>
+                                <h1 class="display-5 mb-4">Cadastro de ONG</h1>
+                                <form>
+                                    <div class="row gy-3 gx-4">
+                                        <div class="col-xl-6">
+                                            <label for="">NOME</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="NOME">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">DOCUMENTO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="DOCUMENTO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">CEP</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="CEP">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">ESTADO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="ESTADO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">CIDADE</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="CIDADE">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">ENDEREÇO</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="ENDEREÇO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">CONTATO</label>
+                                            <input type="phone" class="form-control py-3 border-primary bg-transparent" placeholder="CONTATO">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">EMAIL</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent text-white" placeholder="EMAIL">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <label for="">SENHA</label>
+                                            <input type="text" class="form-control py-3 border-primary bg-transparent" placeholder="SENHA">
+                                        </div>
+                                        <div class="col-12">
+                                            <button type="button" class="btn btn-primary text-white w-100 py-3 px-5">CADASTRAR</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal Video -->
+<div class="modal fade" id="videoModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content rounded-0">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Youtube Video</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- 16:9 aspect ratio -->
+                <div class="ratio ratio-16x9">
+                    <iframe class="embed-responsive-item" src="" id="video" allowfullscreen allowscriptaccess="always" allow="autoplay"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Book Appointment End -->
+
+
+
+<?php include('./rodape.php') ?>
